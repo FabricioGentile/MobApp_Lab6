2dd82b0854a2dcfe3f3bc78915ed5cf47f19986c
FABRICIO DE SOUZA GENTILE - STUDENT <G00363827@gmit.ie>
2019-10-16 10:45:47 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/whatever', (req, res) => res.send('Whatever'))
+
+app.get('/name', (req, res) => {
+    res.send('Hello ' + req.query.firstName + " " + req.query.lastName);
+})
+
+app.post('/name', (req, res) => {
+    res.send('Hello ' + req.body.firstName + " " + req.body.lastName);
+})
+
+app.get('/hello/:name', (req, res) => {
+    console.log(req.params.name);
+    res.send('Hello ' + req.params.name);
+})
+
+app.get('/api/movies', (req, res) => {    
+    const myMovies =[
+        {
+            "movies":[
+            {
+            "Title":"Avengers: Infinity War",
+            "Year":"2018",
+            "Poster":"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+            },
+            {
+            "Title":"Captain America: Civil War",
+            "Year":"2016",
+            "Poster":"https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+            }
+            ]
+        }
+    ]
+    res.status(200).json({movies:myMovies,
+                            message:'Operation completed sucessfully'});
+    res.send('asddas');
+})
+
+app.get('/test', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
